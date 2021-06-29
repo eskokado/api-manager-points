f99786b226f7847547be8c70600723b149905e4f
Edson Shideki Kokado <eskokado@gmail.com>
2021-06-29 11:56:27 -0300

    Initial commit

---
+++ src/main/java/com/dio/santander/apimanagerpoints/ApiManagerPointsApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.apimanagerpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagerPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagerPointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/santander/apimanagerpoints/ApiManagerPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.apimanagerpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiManagerPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
